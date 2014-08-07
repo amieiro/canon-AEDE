e254ddea6802739dc90ad02f693a097ced738983
Jesus Amieiro <amieiro@gmail.com>
2014-08-07 15:03:47 +0200

    Modify the algorithm that search the domains
    Modify the algorithm that search the domains to avoid some problems
    with false positives and another problems when the line starts with
    the blocked domain.

--- canon-AEDE.php
+++ canon-AEDE.php
@@ -30,16 +30,15 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 	$url_base = get_bloginfo('url');
 	$domain_base = str_replace("http://","",get_bloginfo('url'));
 	$domain_base = str_replace("https://","",$domain_base);
-	$reg_exUrl = "!(http|ftp|scp)(s)?:\/\/[a-zA-Z0-9.?&_/=\-]+!"; 
-	$reg_exDomain = "/[\w\d\-\.]+\.[\w\d]{1,3}/";
+	$reg_exUrl = "!(http|ftp|scp)(s)?:\/\/[a-zA-Z0-9.?&_/=\-%#;]+!"; // Original regex
+	$reg_exDomain = "/[\w\d\-\.]+\.[\w\d]{1,3}/"; // Original regex
+	//$reg_exUrl = "/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i"; 
+	//$reg_exDomain = "/[\w\d\-\.]+\.[\w\d]{1,3}[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/";
 
-//print_r($domain_list);
 // Hooks list http://codex.wordpress.org/Plugin_API/Filter_Reference
 add_filter('the_content', 'remove_links');
 add_filter('the_title', 'remove_links');
 ///////////////add_filter('the_permalink', 'remove_links');
-
-
 
 function remove_links($content) {
 	// Remove the URL: http|ftp|scp)(s)
@@ -52,11 +51,22 @@ function remove_links($content) {
 	// Remove the remain domains
 	preg_match_all($GLOBALS['reg_exDomain'], $content, $matches);
     foreach ($matches[0] as $pattern) {
-			if (strposa($pattern,$GLOBALS['domain_list'])) {
-	 	        $content = str_replace_first($pattern, $GLOBALS['domain_base'],$content);
+			if (in_array(str_ireplace('www.', '', parse_url('http://'. $pattern,PHP_URL_HOST)), $GLOBALS['host_list'])) {
+				if (strpos($pattern, "www") === 0) {
+		 	        $content = str_replace_first($pattern, $GLOBALS['domain_base'], $content);		
+				}
+				 else {
+				 		$pos = strpos($content, $pattern); // get the position of the pattern
+				 		$letter_before = substr($content, $pos -1, 1); // get the letter before
+				 		if (($letter_before == "\n") || ($letter_before ==" ") || ($letter_before ==">"))  {
+				 	        $content = str_replace_first($pattern, $GLOBALS['domain_base'], $content);			
+			 	        } else {
+				 	        $content = str_replace_first(' ' . $pattern, ' ' . $GLOBALS['domain_base'], $content);			
+			 	        }	
+				}
             }
     }
-	return $content;
+    return $content;
 }
 
 function strposa($haystack, $needle, $offset=0) {
